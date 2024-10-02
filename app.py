6b9cbc5006ffac9647d116c0fa229ff5de3b1238
anna <anniegabriento51@gmail.com>
2024-10-02 14:53:53 -0700

    removed unnecessary pages

--- app.py
+++ app.py
@@ -17,20 +17,20 @@ def index():
 @app.route('/wifidog/login/', methods = ['GET', 'POST'], strict_slashes=False)
 @app.route('/login', methods = ['GET', 'POST'], strict_slashes=False)
 def login():
-    gw_id = request.args.get('gw_id', 'mpop9016MP')
-    gw_sn = request.args.get('gw_sn', 'mpop9016MP')
-    gw_address = request.args.get('gw_address', '1.2.3.4')
-    gw_port = request.args.get('gw_port', '2060')
-    ip = request.args.get('ip', '10.51.0.62')
-    mac = request.args.get('mac', '58:fb:84:78:69:fa')
-    apmac = request.args.get('apmac', '0074.9c65.8d2c')
-    ssid = request.args.get('ssid', 'TEST PORTAL 2')
-    vlanid = request.args.get('vlanid', '51')
-    url = request.args.get('url', 'http://www.msftconnecttest.com/redirect')
+    gw_id = request.args.get('gw_id', default='', type=str)
+    gw_sn = request.args.get('gw_sn', default='', type=str)
+    gw_address = request.args.get('gw_address', default='', type=str)
+    gw_port = request.args.get('gw_port', default='', type=str)
+    ip = request.args.get('ip', default='', type=str)
+    mac = request.args.get('mac', default='', type=str)
+    apmac = request.args.get('apmac', default='', type=str)
+    ssid = request.args.get('ssid', default='', type=str)
+    vlanid = request.args.get('vlanid', default='', type=str)
+    
 
     return render_template("index.html", gw_id=gw_id, gw_sn=gw_sn, gw_address=gw_address, 
                            gw_port=gw_port, ip=ip, mac=mac, apmac=apmac, ssid=ssid, 
-                           vlanid=vlanid, url=url)
+                           vlanid=vlanid)
 
 @app.route('/dashboard')
 def dashboard():
