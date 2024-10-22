1e7d14cc6192780757a286d84570e9fc8aa133e3
jlls12 <jeezasalise@gmail.com>
2024-10-22 15:47:46 +0800

    updated changes to app.py
    fixing conflicts

--- config.py
+++ config.py
@@ -5,4 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 FLASK_DEBUG = os.environ.get("FLASK_DEBUG")
+<<<<<<< HEAD
 SECRET_KEY = os.environ.get("SECRET_KEY")
+=======
+SECRET_KEY = os.environ.get("SECRET_KEY")
+>>>>>>> b3a7ade (updated changes to app.py)
